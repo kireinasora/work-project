7dcb91a1db14b0552b5d29e1991e724073ec333f
Kin <kireinalunar@gmail.com>
2020-05-01 14:20:11 +0800

    sync mac

--- webpage/src/main.js
+++ webpage/src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 

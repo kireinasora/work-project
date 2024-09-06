d771419b0aacff5c6b3d6926975b98cc5a68fce8
kireinasora <kireinalunar@gmail.com>
2024-09-06 19:41:09 +0800

    first

--- app.py
+++ app.py
@@ -97,4 +97,5 @@ def generate_excel(form_data):
     return temp_file.name
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)

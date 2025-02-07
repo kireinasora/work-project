019dfcf2ee2ff5416716168d31dd91f20acab88f
unknown <kireinasora>
2025-02-07 20:10:02 +0800

    修改main.py配合heroku

--- backend/main.py
+++ backend/main.py
@@ -55,5 +55,7 @@ def create_app():
 
 
 if __name__ == "__main__":
+    import os
     app = create_app()
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
